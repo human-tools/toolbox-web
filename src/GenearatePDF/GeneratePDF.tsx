2c9b980cfdaac288a5aa83e1955b81b99c0ab587
Ahmad Tolba <tolpa1@gmail.com>
2021-04-09 21:55:50 +0200

    Fixes #7 (#9)

--- src/GenearatePDF/GeneratePDF.tsx
+++ src/GenearatePDF/GeneratePDF.tsx
@@ -51,8 +51,8 @@ const GeneratePDFFromImages = (): JSX.Element => {
   const [pdf, setPDF] = useState<PDFDocumentProxy>();
   const [doc, setDoc] = useState<PDFDocument>();
   const {
-    orderedItems: orderedPages,
-    setItems: setPages,
+    orderedItems: pagesOrder,
+    setItems: setPagesOrder,
     setContainerRef,
     addDraggableNodeRef,
   } = useSortable<number>([], {
@@ -70,22 +70,31 @@ const GeneratePDFFromImages = (): JSX.Element => {
         files,
         doc
       );
+
+      // Calculate new page order with the added pages at the end of the order.
+      // To avoid losing previously ordered indexes.
       const pdf = (await getDocument(bytes).promise) as PDFDocumentProxy;
+      const oldPagesOrder = pagesOrder;
+      const oldPagesCount = oldPagesOrder.length;
+      const newlyAddedPagesOrder = new Array(pageCount - oldPagesCount)
+        .fill(0)
+        .map((_, index) => oldPagesCount + index + 1);
+      const newOrder = [...oldPagesOrder, ...newlyAddedPagesOrder];
       setDoc(newPdfDoc);
       setPDF(pdf);
-      setPages(new Array(pageCount).fill(0).map((_, index) => index + 1));
+      setPagesOrder(newOrder);
     },
-    [setPages, doc]
+    [setPagesOrder, doc, pagesOrder]
   );
 
   const onSave = useCallback(async () => {
     if (!doc) return;
-    const { bytes } = await getOrderedPdf(doc, orderedPages);
+    const { bytes } = await getOrderedPdf(doc, pagesOrder);
     saveAs(
       new Blob([bytes]),
       fileName.endsWith('.pdf') ? fileName : `${fileName}.pdf`
     );
-  }, [doc, fileName, orderedPages]);
+  }, [doc, fileName, pagesOrder]);
 
   return (
     <div className="h-full flex flex-col">
@@ -110,13 +119,13 @@ const GeneratePDFFromImages = (): JSX.Element => {
               fullSized={!pdf}
             />
           </div>
-          {pdf && pdf.numPages === orderedPages.length && (
+          {pdf && pdf.numPages === pagesOrder.length && (
             <div className="flex flex-col flex-grow">
               <div
                 className="flex p-2 flex-wrap flex-grow-1 h-full my-1 items-start content-start justify-center lg:justify-start"
                 ref={setContainerRef}
               >
-                {orderedPages.map((pageNumber: number) => (
+                {pagesOrder.map((pageNumber: number) => (
                   <div
                     ref={addDraggableNodeRef}
                     key={pageNumber}
