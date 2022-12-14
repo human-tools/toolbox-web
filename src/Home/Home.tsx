0c674596f3eb0c490974030355933a70fa4c9d60
Mohammad Khatib <mkhatib727@gmail.com>
2022-12-14 09:58:51 -0800

    Update UI Styles (#65)
    * Update Home page and upload button styles
    
    * Update styling for Combine PDF, Slideshow and Quick Sign styling
    
    * Update Bulk Edit Photos styling

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,45 +1,37 @@
-import React from 'react';
-import goggles from '../assets/goggles.png';
 import ToolsNav from '../ToolsNav/ToolsNav';
 
 const Home = (): JSX.Element => {
   return (
     <div className="p-4">
       <article className="flex flex-col lg:flex-row lg:flex-row-reverse	lg:justify-end lg:items-center lg:px-10">
-        <div className="lg:flex-1 lg:self-center">
-          <section className="flex justify-center">
-            <img
-              src={goggles}
-              className="max-w-xs max-h-xs md:max-w-md md:max-h-md lg:max-w-xl lg:max-h-xl"
-            />
-          </section>
-        </div>
-        <div className="lg:text-xl 2xl:text-2xl">
+        <div className="lg:text-xl 2xl:text-xl">
           <section className="mb-4 lg:mb-8">
-            <h1 className="text-2xl font-bold lg:text-4xl">Human Tools</h1>
-            <p>
+            <h1 className="text-2xl font-extrabold lg:text-5xl my-10">
+              Human Tools
+            </h1>
+            <p className="font-extralight">
               A collection of simple utility tools that has data privacy and
               security in mind.
             </p>
-            <div className="my-2">
+            <div className="my-10 flex flex-wrap">
               <ToolsNav
                 showHome={false}
-                className="text-xs p-1 mr-1 inline-block mb-1 bg-blue-300 lg:text-sm lg:p-2 lg:m-1 hover:bg-blue-500"
+                className="font-light inline-flex w-60 h-20 items-center justify-center text-xs p-10 shadow text-center lg:text-xl lg:p-2 lg:m-2 hover:bg-gray-100"
                 activeClassName=""
               />
             </div>
           </section>
-          <section className="mb-4 lg:mb-8">
+          <section className="mb-4 lg:mb-10">
             <h2 className="text-md font-bold">Guiding Principles</h2>
-            <ul className="list-disc ml-10">
+            <ul className="list-disc ml-10 font-extralight">
               <li>No user data is ever uploaded to any server</li>
               <li>All processing happens on users devices</li>
               <li>No need to install software on your devices</li>
             </ul>
           </section>
-          <section className="mb-4 lg:mb-8">
+          <section className="mb-4 lg:mb-10">
             <h2 className="text-md font-bold">Come Learn and Build with Us</h2>
-            <p>
+            <p className="font-extralight">
               Feel free to get in touch on{' '}
               <a
                 className="underline"
@@ -53,7 +45,7 @@ const Home = (): JSX.Element => {
           </section>
           <section className="mb-4 lg:mb-8">
             <h2 className="text-md font-bold">Credits</h2>
-            <ul className="list-disc ml-10">
+            <ul className="list-disc ml-10 font-extralight">
               <li>
                 <a
                   className="underline"
