1e101381be638ea8dd5e662bce82bb35cf2451ad
Mohammed Khatib <mkhatib727@gmail.com>
2023-01-05 15:00:06 -0800

    Add Vercel Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { inject } from '@vercel/analytics/*';
 import 'react-image-crop/dist/ReactCrop.css';
 import './assets/main.generated.css';
 
@@ -17,3 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+inject();
