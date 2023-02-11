69fecbd0fa787e2a49231c7d1de80d530df3a620
Ahmad <tolpa1@gmail.com>
2023-02-11 17:12:12 +0100

    Change style (#97)

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -6,23 +6,23 @@ const Home = (): JSX.Element => {
       <article className="flex flex-col lg:flex-row lg:flex-row-reverse	lg:justify-end lg:items-center lg:px-10">
         <div className="lg:text-xl 2xl:text-xl">
           <section className="mb-4 lg:mb-8">
-            <h1 className="text-2xl font-extrabold lg:text-5xl my-10">
+            <h1 className="text-2xl font-medium lg:text-5xl my-10">
               Human Tools
             </h1>
-            <p className="font-extralight">
+            <p className="font-light">
               A collection of simple utility tools that has data privacy and
               security in mind.
             </p>
             <div className="my-10 flex flex-wrap">
               <ToolsNav
                 showHome={false}
-                className="font-light inline-flex w-60 h-20 items-center justify-center text-xs p-10 shadow text-center lg:text-xl lg:p-2 lg:m-2 hover:bg-gray-100"
+                className="font-light inline-flex w-60 h-20 items-center justify-center text-xs p-10 shadow rounded-lg text-center lg:text-xl lg:p-2 lg:m-2 bg-gradient-to-r hover:from-gray-100 hover:to-cian-500"
                 activeClassName=""
               />
             </div>
           </section>
           <section className="mb-4 lg:mb-10">
-            <h2 className="text-md font-bold">Guiding Principles</h2>
+            <h2 className="text-md font-medium">Guiding Principles</h2>
             <ul className="list-disc ml-10 font-extralight">
               <li>No user data is ever uploaded to any server</li>
               <li>All processing happens on users devices</li>
@@ -30,7 +30,9 @@ const Home = (): JSX.Element => {
             </ul>
           </section>
           <section className="mb-4 lg:mb-10">
-            <h2 className="text-md font-bold">Come Learn and Build with Us</h2>
+            <h2 className="text-md font-medium">
+              Come Learn and Build with Us
+            </h2>
             <p className="font-extralight">
               Feel free to get in touch on{' '}
               <a
@@ -44,7 +46,7 @@ const Home = (): JSX.Element => {
             </p>
           </section>
           <section className="mb-4 lg:mb-8">
-            <h2 className="text-md font-bold">Credits</h2>
+            <h2 className="text-md font-medium">Credits</h2>
             <ul className="list-disc ml-10 font-extralight">
               <li>
                 <a
